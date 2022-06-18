9457cd9b27d6c2229485005bfe654f78e5566118
DrTamTam <102097588+DrTamTam@users.noreply.github.com>
2022-06-18 15:58:02 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const form = document.getElementById("form");
+const fname = document.getElementById("fname");
+const lname = document.getElementById("lname");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    checkInputs();
+});
+
+function checkInputs() {
+    const fnameValue = fname.value.trim();
+    const lnameValue = lname.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+    if(fnameValue === "") {
+        setErrorFor(fname, "First Name cannot be empty");
+    } else {
+        setSuccessFor(fname);
+    }
+
+    if(lnameValue === "") {
+        setErrorFor(lname, "Last Name cannot be empty");
+    }
+
+    if(emailValue === "") {
+        setErrorFor(email, "Looks like this is not an email");
+    }
+
+    if(passwordValue === "") {
+        setErrorFor(password, "Password cannot be empty");
+    }
+}
+
+function setErrorFor(input, message) {
+    const inputField = input.parentElement;
+    const small = inputField.querySelector("small");
+
+    small.innerText = message;
+
+    inputField.className = "input-field error";
+}
+document.g
